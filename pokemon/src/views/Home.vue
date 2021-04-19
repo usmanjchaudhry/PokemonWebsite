29451da84ce5a453d37037d2da1d3b7a8c7006b7
Jpreming <56087406+jpreming@users.noreply.github.com>
2021-04-18 20:54:39 -0500

    Routing+bootstrap

---
+++ pokemon/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+<form method="post">
+	<div class="form-group">
+    	<label for="Username">Username</label>
+		<input type="text" class="form-control" name="username" placeholder="Username" required="true">  
+	</div>
+    <div class="form-group">
+    	<label for="Password">Password</label>
+		<input type="password" class="form-control" name="password" placeholder="Password" required="true"> 
+	</div>
+    <button type="submit" class="btn btn-primary">Login</button>
+</form>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Home",
+  components: {
+    
+  },
+};
+</script>
