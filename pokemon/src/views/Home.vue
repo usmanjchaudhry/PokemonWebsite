68f7a3336e1a9598e7bfad4bdf346848121e1c8a
usmanjchaudhry <36524989+usmanjchaudhry@users.noreply.github.com>
2021-05-19 19:27:05 -0700

    redone whole home page

--- pokemon/src/views/Home.vue
+++ pokemon/src/views/Home.vue
@@ -3,9 +3,17 @@
 
 
 <div class="fvue">
-  <img class="catch" src="https://cdn.freebiesupply.com/logos/thumbs/2x/pokemon-23-logo.png">
+  <img class="catch" src="https://www.freepnglogos.com/uploads/pokemon-logo-text-png-7.png">
 </div>
 
+
+
+<div class="fvue2">
+  <img class="catch9" href ="about" src="https://www.pngkey.com/png/full/56-566109_pikachu-transparent-background-pokmon-pikachu.png">
+</div>
+
+
+<h1 class="text">Click me to view all pokemon</h1>
 
 </div>
 
@@ -33,15 +41,21 @@ export default {
 <style scoped>
 
 .fvue {
-  margin-bottom:0px;
-  margin-top: -200px;
- align-content: center;
+  margin-bottom:-400px;
+  margin-top: 0px;
+text-align: center;
   
 }
-
+.fvue2 {
+margin-top: 300px;
+text-align: center;
+ animation: fadeIn;
+  animation-duration:5s;
+  
+}
 .catch {
 
-  height:900px;
+  height:300px;
   width:1000px;
 text-align: center;
   align-content: center;
@@ -50,5 +64,10 @@ text-align: center;
  
 }
 
-
+.text{
+ animation: fadeIn;
+  animation-duration:3s;
+  font-size: 40px;
+  text-align: center;
+}
 </style>
